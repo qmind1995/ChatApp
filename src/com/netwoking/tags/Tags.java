b1ad617dd66d5a535b52219b45eacf6707ca0817
Q.VINH <qvinh@QVINHs-MacBook-Pro.local>
2014-09-20 14:41:54 +0700

    Update Sep 20th

---
+++ src/com/netwoking/tags/Tags.java
@@ -0,0 +1,22 @@
+package com.netwoking.tags;
+
+public class Tags {
+
+	public static String SESSION_OPEN_TAG = "<SESSION>";
+	public static String SESSION_CLOSE_TAG = "</SESSION>";
+	public static String PEER_NAME_OPEN_TAG = "<PEER_NAME>";
+	public static String PEER_NAME_CLOSE_TAG = "</PEER_NAME>";
+	public static String PORT_OPEN_TAG = "<PORT>";
+	public static String PORT_CLOSE_TAG = "</PORT>";
+	public static String SESSION_KEEP_ALIVE_OPEN_TAG = "<SESSION_KEEP_ALIVE>";
+	public static String SESSION_KEEP_ALIVE_CLOSE_TAG = "</SESSION_KEEP_ALIVE>";
+	public static String STATUS_OPEN_TAG = "<STATUS>";
+	public static String STATUS_CLOSE_TAG = "</STATUS>";
+
+	public static String PEER_OPEN_TAG = "<PEER>";
+	public static String PEER_CLOSE_TAG = "</PEER>";
+
+	// ------------------
+	public static String ALIVE = "ALIVE";
+
+}
